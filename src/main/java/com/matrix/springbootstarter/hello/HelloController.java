dd25865b05a8fe86863676e0a2b830a6968ec39c
Matrix <matrix@macbook.local>
2019-05-21 22:52:44 +0530

    First commit

---
+++ src/main/java/com/matrix/springbootstarter/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.matrix.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+	
+	
+	@RequestMapping("/hello")
+	public String sayHi()
+	{
+		return "Hi";
+	}
+
+}
